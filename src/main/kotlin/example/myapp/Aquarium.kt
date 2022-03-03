819f5a3a9c83f966a645c5d2c992c949886fa902
Derek <derekwellis@outlook.com>
2022-03-03 11:18:16 -0600

    changed for new code lab on classes

---
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -0,0 +1,54 @@
+package example.myapp
+
+import java.lang.Math.PI
+
+open class Aquarium (var length : Int = 100, var width : Int = 20, open var height : Int = 40) {
+
+    constructor(numberOfFish : Int) : this() {
+        // 2000 cm^3 per fish plus extra
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
+
+    open var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    open val shape = "rectangle"
+
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
+    init {
+        println("aquarium initializing...")
+    }
+    init {
+        println("Volume: ${width * length * height / 1000} l")
+    }
+
+    fun printSize() {
+        println("Width: $width cm\n" +
+                "Length: $length cm\n" +
+                "Height: $height cm\n"
+        )
+        // 1 l = 1000 cm^3
+        println("Volume: $volume l")
+    }
+
+
+    class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+        override var volume: Int
+            // ellipse area = π * r1 * r2
+            get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+            set(value) {
+                height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+            }
+
+        override var water = volume * 0.8
+        override val shape = "cylinder"
+    }
+
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import example.myapp.Aquarium
 import java.util.*
 
 // all worked from google code labs kotlin bootcamp for programmers: functions
@@ -130,4 +131,33 @@ fun main(args: Array<String>) {
     dirtyLevel = 19;
     dirtyLevel = updateDirty(dirtyLevel) { dirtyLevel -> dirtyLevel + 23}
     println(dirtyLevel)
+
+
+    fun buildAquarium() {
+        val aquarium1 = Aquarium()
+        aquarium1.printSize()
+        // default height and length
+        val aquarium2 = Aquarium(width = 25)
+        aquarium2.printSize()
+        // default width
+        val aquarium3 = Aquarium(height = 35, length = 110)
+        aquarium3.printSize()
+        // everything custom
+        val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+        aquarium4.printSize()
+
+        val aquarium6 = Aquarium(numberOfFish = 29)
+        aquarium6.printSize()
+        aquarium6.volume = 70
+        aquarium6.printSize()
+        println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} l")
+
+        val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+        myAquarium.printSize()
+        val myTower = Aquarium.TowerTank(diameter = 25, height = 40)
+        myTower.printSize()
+    }
+
+    buildAquarium()
+
 }
